1e30b7d19887639f5d11de65fb80a2e7a2ba325a
CodnanBaig <adnanbaigofficial@gmail.com>
2023-09-14 23:54:28 +0530

    Merged

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -5,11 +5,14 @@ import Checkout from "./Pages/Checkout";
 import Successfull from "./Pages/Success";
 import { Home } from "./Pages/Home";
 import { DestinationDetails } from "./Pages/DestinationDetails";
+import AuthPage from './Pages/AuthPage/AuthPage'
+
 const AllRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/auth" element={<AuthPage/>}></Route>
         <Route path="/destination/test" element={<DestinationDetails/>}/>
         <Route path="/checkout" element={<Checkout/>}></Route>
         <Route path="/payment" element={<Payment />}></Route>
